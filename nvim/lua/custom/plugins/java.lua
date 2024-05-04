779b9401d55fa3b277c5bd21eaa7134ac1b510ad
Casper Szymiczek-Graley <casper.szy@gmail.com>
2024-05-04 17:42:42 +1000

    feat: update Lazy and java plugin

--- nvim/lua/custom/plugins/java.lua
+++ nvim/lua/custom/plugins/java.lua
@@ -1,26 +1,27 @@
 return {
   -- TODO fix java
-  'nvim-java/nvim-java',
+  "nvim-java/nvim-java",
   dependencies = {
-    'nvim-java/lua-async-await',
-    'nvim-java/nvim-java-core',
-    'nvim-java/nvim-java-test',
-    'nvim-java/nvim-java-dap',
-    'MunifTanjim/nui.nvim',
-    'neovim/nvim-lspconfig',
-    'mfussenegger/nvim-dap',
+    "nvim-java/lua-async-await",
+    "nvim-java/nvim-java-core",
+    "nvim-java/nvim-java-test",
+    "nvim-java/nvim-java-dap",
+    "MunifTanjim/nui.nvim",
+    "neovim/nvim-lspconfig",
+    "mfussenegger/nvim-dap",
+    "nvim-java/nvim-java-refactor",
     {
-      'williamboman/mason.nvim',
+      "williamboman/mason.nvim",
       opts = {
         registries = {
-          'github:nvim-java/mason-registry',
-          'github:mason-org/mason-registry',
+          "github:nvim-java/mason-registry",
+          "github:mason-org/mason-registry",
         },
       },
     },
   },
   config = function()
-    require('java').setup()
-    require('lspconfig').jdtls.setup {}
+    require("java").setup()
+    require("lspconfig").jdtls.setup({})
   end,
 }
