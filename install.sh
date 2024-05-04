d11f6c08288c7e661a5046332f9da20d2f988b56
Casper Szymiczek-Graley <casper.szy@gmail.com>
2024-05-04 11:55:51 +1000

    feat: fix install.sh, and add general .editorconfig

--- install.sh
+++ install.sh
@@ -2,19 +2,30 @@
 ############################
 # This script creates symlinks from the home directory to any desired dotfiles
 ############################
+mv ~/.zsh_aliases ~/.zsh_aliases.bak
+mv ~/.zsh_init ~/.zsh_init.bak
+mv ~/.zsh_theme ~/.zsh_theme.bak
+mv ~/.p10k.zsh ~/.p10k.zsh.bak
+mv ~/.gitmessage ~/.gitmessage.bak
+# mv ~/.gitconfig ~/.gitconfig.bak
+mv ~/.vimrc ~/.vimrc.bak
+mv ~/.config/nvim ~/.config/nvim.bak
+mv ~/.editorconfig ~/.editorconfig.bak
 
 # assumes zsh
-ln -s $PWD/zsh/.zsh_aliases ~/.zsh_aliases
-ln -s $PWD/zsh/.zsh_init ~/.zsh_init
-ln -s $PWD/zsh/.zsh_theme ~/.zsh_theme
-ln -s $PWD/zsh/.p10k.zsh ~/.p10k.zsh
+ln -s "$(pwd)/zsh/.zsh_aliases" ~/.zsh_aliases
+ln -s "$(pwd)/zsh/.zsh_init" ~/.zsh_init
+ln -s "$(pwd)/zsh/.zsh_theme" ~/.zsh_theme
+ln -s "$(pwd)/zsh/.p10k.zsh" ~/.p10k.zsh
 
-ln -s $PWD/git/.gitmessage ~/.gitmessage
-# ln -s $PWD/git/.gitconfig ~/.gitconfig
+ln -s "$(pwd)/git/.gitmessage" ~/.gitmessage
+# ln -s "$(pwd)/git/.gitconfig" ~/.gitconfig
 
-ln -s $PWD/vim/.vimrc ~/.vimrc
+ln -s "$(pwd)/vim/.vimrc" ~/.vimrc
 mkdir -p ~/.config
-ln -s $PWD/nvim ~/.config/nvim
+ln -s "$(pwd)/nvim" ~/.config/nvim
 
-$PWD/git/git_aliases.sh
+ln -s "$(pwd)/.editorconfig" ~/.editorconfig
+
+$(pwd)/git/git_aliases.sh
 
