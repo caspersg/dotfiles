21df30eb3477993863f83909c661bc4956426ca3
Casper Szymiczek-Graley <casper.szy@gmail.com>
2024-06-02 19:45:56 +1000

    feat: add back marks to show mark name in gutter

--- nvim/lua/custom/plugins/marks.lua
+++ nvim/lua/custom/plugins/marks.lua
@@ -1,9 +1,10 @@
 return {
-  -- 'chentoast/marks.nvim',
-  -- config = function()
-  --   require('marks').setup {
-  --     -- just use the built in mappings
-  --     default_mappings = true,
-  --   }
-  -- end,
+  "chentoast/marks.nvim",
+  config = function()
+    require("marks").setup({
+      -- just use the built in mappings
+      default_mappings = true,
+      force_write_shada = true,
+    })
+  end,
 }
