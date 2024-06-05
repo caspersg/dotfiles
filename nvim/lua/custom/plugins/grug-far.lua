1ea4db4313eb747e8dacc180f45508257bbaaa3d
Casper Szymiczek-Graley <cszymiczek-graley@zendesk.com>
2024-06-05 18:16:21 +1000

    feat: switch to grug-far over spectre for search and replace

--- nvim/lua/custom/plugins/grug-far.lua
+++ nvim/lua/custom/plugins/grug-far.lua
@@ -4,5 +4,21 @@ return {
     require("grug-far").setup({
       windowCreationCommand = "tabnew",
     })
+
+    vim.keymap.set("n", "<leader>sr", function()
+      require("grug-far").grug_far({})
+    end, { desc = "[s]earch [r]eplace" })
+
+    vim.keymap.set("v", "<leader>sv", function()
+      require("grug-far").with_visual_selection({})
+    end, {
+      desc = "[s]earch [v]isual current word",
+    })
+
+    vim.keymap.set("n", "<leader>sc", function()
+      require("grug-far").grug_far({ prefills = { flags = vim.fn.expand("%") } })
+    end, {
+      desc = "[s]earch in [c]urrent file",
+    })
   end,
 }
