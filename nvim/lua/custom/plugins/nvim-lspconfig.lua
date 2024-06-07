25feb94e5780614c6616d7f5c2d7c26a9038b6f0
Casper Szymiczek-Graley <cszymiczek-graley@zendesk.com>
2024-06-07 10:43:50 +1000

    fix: pyright not being able to find imports

--- nvim/lua/custom/plugins/nvim-lspconfig.lua
+++ nvim/lua/custom/plugins/nvim-lspconfig.lua
@@ -284,17 +284,25 @@ return {
       },
     })
 
+    local lspconfig = require("lspconfig")
+
     -- ruff_lsp
     -- Configure `ruff-lsp`.
     -- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#ruff_lsp
     -- For the default config, along with instructions on how to customize the settings
-    require("lspconfig").ruff_lsp.setup({
+    lspconfig.ruff_lsp.setup({
       init_options = {
         settings = {
           -- Any extra CLI arguments for `ruff` go here.
           args = {},
         },
       },
+    })
+
+    lspconfig.pyright.setup({
+      root_dir = function(fname)
+        return lspconfig.util.root_pattern(".git", "pyproject.toml")(fname)
+      end,
     })
   end,
 }
